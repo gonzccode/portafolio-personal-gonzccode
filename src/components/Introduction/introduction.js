5ef9a895c34ed5c74655388305280feb2777fc88
GonzaCC <acanazac@uni.pe>
2023-04-13 17:56:36 -0500

    Portafolio finalizado v1

--- src/components/Introduction/introduction.js
+++ src/components/Introduction/introduction.js
@@ -21,42 +21,33 @@ const Introduction = () => {
                 <Spinner className='spinnerLoading' animation="grow"  role="status" variant="light"/>
             </div> 
             :
-            <div id="introduction">
-                    <div className='introductionText'>
-                        <h4>
-                            Hola 👋, soy <strong>Gonzalo</strong>
-                        </h4>
-                        <h3>
-                            <strong>Desarrollador Frontend</strong>
-                        </h3>
-                        <p>
-                            Apasionado por la tecnología y <br></br> el desarrollo de software.
-                        </p>
-                        <div className='introductionButton'>
-                            <a className='btnProject' href='/proyectos'>
-                                Proyectos
-                            </a>
-                            <a className='btnContact' href='/contacto'>
-                                Contáctame
-                            </a>
+            <section id='portfolio'>
+                <div id="introduction">
+                        <div className='introductionText'>
+                            <h4>
+                                Hola 👋, soy <strong>Gonzalo</strong>
+                            </h4>
+                            <h3>
+                                <strong>Desarrollador Frontend</strong>
+                            </h3>
+                            <p>
+                                Apasionado por la tecnología y <br></br> el desarrollo de software.
+                            </p>
+                            <div className='introductionButton'>
+                                <a className='btnProject' href='/proyectos'>
+                                    Proyectos
+                                </a>
+                                <a className='btnContact' href='/contacto'>
+                                    Contáctame
+                                </a>
 
+                            </div>
                         </div>
-                    </div>
-                    <div className='introductionImage'>
-                        <img src={ImagePortfolio} className="imagePortfolio" alt="portafolio" />
-                    </div>
-                
-                
-                
-                {/* <a
-                    className="introductionLink"
-                    href="https://github.com/gonzccode"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                GitHub
-                </a> */}
-            </div>
+                        <div className='introductionImage'>
+                            <img src={ImagePortfolio} className="imagePortfolio" alt="portafolio" />
+                        </div>
+                </div>
+            </section>
             }
         </>
     )
