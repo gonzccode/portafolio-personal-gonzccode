bf757d74f511054f281d757c1df76869c5f83c68
GonzaCC <acanazac@uni.pe>
2023-04-13 01:21:40 -0500

    agregando spinner

--- src/components/Introduction/introduction.js
+++ src/components/Introduction/introduction.js
@@ -1,45 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import ImagePortfolio from '../../assets/img/image-portafolio.png';
 
 const Introduction = () => {
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect (() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+    }, []);
+
+
     return (
-        <div id="introduction">
-                <div className='introductionText'>
-                    <h4>
-                        Hola 👋, soy <strong>Gonzalo</strong>
-                    </h4>
-                    <h3>
-                        <strong>Desarrollador Frontend</strong>
-                    </h3>
-                    <p>
-                        Apasionado por la tecnología y <br></br> el desarrollo de software.
-                    </p>
-                    <div className='introductionButton'>
-                        <a className='btnProject' href='/proyectos'>
-                            Proyectos
-                        </a>
-                        <a className='btnContact' href='/contacto'>
-                            Contáctame
-                        </a>
+        <>
+            {loading ? 
+            <div id='spinnerSection'>
+                <Spinner className='spinnerLoading' animation="grow"  role="status" variant="light"/>
+            </div> 
+            :
+            <div id="introduction">
+                    <div className='introductionText'>
+                        <h4>
+                            Hola 👋, soy <strong>Gonzalo</strong>
+                        </h4>
+                        <h3>
+                            <strong>Desarrollador Frontend</strong>
+                        </h3>
+                        <p>
+                            Apasionado por la tecnología y <br></br> el desarrollo de software.
+                        </p>
+                        <div className='introductionButton'>
+                            <a className='btnProject' href='/proyectos'>
+                                Proyectos
+                            </a>
+                            <a className='btnContact' href='/contacto'>
+                                Contáctame
+                            </a>
 
+                        </div>
                     </div>
-                </div>
-                <div className='introductionImage'>
-                    <img src={ImagePortfolio} className="imagePortfolio" alt="portafolio" />
-                </div>
-            
-            
-            
-            {/* <a
-                className="introductionLink"
-                href="https://github.com/gonzccode"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-            GitHub
-            </a> */}
-        </div>
-
+                    <div className='introductionImage'>
+                        <img src={ImagePortfolio} className="imagePortfolio" alt="portafolio" />
+                    </div>
+                
+                
+                
+                {/* <a
+                    className="introductionLink"
+                    href="https://github.com/gonzccode"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                GitHub
+                </a> */}
+            </div>
+            }
+        </>
     )
     
 }
--- src/components/Technologies/technologies.js
+++ src/components/Technologies/technologies.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import IconHtml from '../../assets/img/icon-html.png'
 import IconCss from '../../assets/img/icon-css.png'
 import IconJs from '../../assets/img/icon-js.png'
@@ -18,87 +19,104 @@ import IconFlask from '../../assets/img/icon-flask.png'
 import IconPostgresql from '../../assets/img/icon-postgresql.png'
 
 const Technologies = () => {
-  return (
-    <div id='technologies'>
-        <h3>
-            <strong>
-                 &lt;Tecnologías/&gt;
-            </strong>
-        </h3>
-        <br/>
-        <br/>
-        <div className='iconsTechnologies'>
-            <div>
-                <img src={IconHtml} className='iconTechnologie' alt='html'/>
-                <p>HTML</p>
+    const [loading, setLoading] = useState(true);
+
+    useEffect (() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+    }, []);
+
+    return (
+        <>
+            {loading ? 
+            <div id='spinnerSection'>
+                <Spinner className='spinnerLoading' animation="grow"  role="status" variant="light"/>
+            </div> 
+            :
+            <div id='technologies'>
+                <h3>
+                    <strong>
+                        &lt;Tecnologías/&gt;
+                    </strong>
+                </h3>
+                <br/>
+                <br/>
+                <div className='iconsTechnologies'>
+                    <div>
+                        <img src={IconHtml} className='iconTechnologie' alt='html'/>
+                        <p>HTML</p>
+                    </div>
+                    <div>
+                        <img src={IconCss} className='iconTechnologie' alt='css'/>
+                        <p>CSS</p>
+                    </div>
+                    <div>
+                        <img src={IconJs} className='iconTechnologie' alt='js'/>
+                        <p>Javascript</p>
+                    </div>
+                    <div>
+                        <img src={IconBootstrap} className='iconTechnologie' alt='bootstrap'/>
+                        <p>Bootstrap</p>
+                    </div>
+                    <div>
+                        <img src={IconSass} className='iconTechnologie' alt='sass'/>
+                        <p>Sass</p>
+                    </div>
+                    <div>
+                        <img src={IconGit} className='iconTechnologie' alt='git'/>
+                        <p>Git</p>
+                    </div>
+                    <div>
+                        <img src={IconReact} className='iconTechnologie' alt='react'/>
+                        <p>React</p>
+                    </div>
+                    <div>
+                        <img src={IconVue} className='iconTechnologie' alt='vue'/>
+                        <p>Vue</p>
+                    </div>
+                    <div>
+                        <img src={IconNode} className='iconTechnologie' alt='node'/>
+                        <p>Node</p>
+                    </div>
+                    <div>
+                        <img src={IconExpress} className='iconTechnologie' alt='express'/>
+                        <p>Express</p>
+                    </div>
+                    <div>
+                        <img src={IconHandlebars} className='iconTechnologie' alt='handlebars'/>
+                        <p>Handlebars</p>
+                    </div>
+                    <div>
+                        <img src={IconMongo} className='iconTechnologie' alt='mongo'/>
+                        <p>MongoDB</p>
+                    </div>
+                    <div>
+                        <img src={IconFirebase} className='iconTechnologie' alt='firebase'/>
+                        <p>Firebase</p>
+                    </div>
+                    <div>
+                        <img src={IconMysql} className='iconTechnologie' alt='mysql'/>
+                        <p>MySQL</p>
+                    </div>
+                    <div>
+                        <img src={IconPython} className='iconTechnologie' alt='python'/>
+                        <p>Python</p>
+                    </div>
+                    <div>
+                        <img src={IconFlask} className='iconTechnologie' alt='flask'/>
+                        <p>Flask</p>
+                    </div>
+                    <div>
+                        <img src={IconPostgresql} className='iconTechnologie' alt='postgresql'/>
+                        <p>PostgreSQL</p>
+                    </div>
+                </div>
             </div>
-            <div>
-                <img src={IconCss} className='iconTechnologie' alt='css'/>
-                <p>CSS</p>
-            </div>
-            <div>
-                <img src={IconJs} className='iconTechnologie' alt='js'/>
-                <p>Javascript</p>
-            </div>
-            <div>
-                <img src={IconBootstrap} className='iconTechnologie' alt='bootstrap'/>
-                <p>Bootstrap</p>
-            </div>
-            <div>
-                <img src={IconSass} className='iconTechnologie' alt='sass'/>
-                <p>Sass</p>
-            </div>
-            <div>
-                <img src={IconGit} className='iconTechnologie' alt='git'/>
-                <p>Git</p>
-            </div>
-            <div>
-                <img src={IconReact} className='iconTechnologie' alt='react'/>
-                <p>React</p>
-            </div>
-            <div>
-                <img src={IconVue} className='iconTechnologie' alt='vue'/>
-                <p>Vue</p>
-            </div>
-            <div>
-                <img src={IconNode} className='iconTechnologie' alt='node'/>
-                <p>Node</p>
-            </div>
-            <div>
-                <img src={IconExpress} className='iconTechnologie' alt='express'/>
-                <p>Express</p>
-            </div>
-            <div>
-                <img src={IconHandlebars} className='iconTechnologie' alt='handlebars'/>
-                <p>Handlebars</p>
-            </div>
-            <div>
-                <img src={IconMongo} className='iconTechnologie' alt='mongo'/>
-                <p>MongoDB</p>
-            </div>
-            <div>
-                <img src={IconFirebase} className='iconTechnologie' alt='firebase'/>
-                <p>Firebase</p>
-            </div>
-            <div>
-                <img src={IconMysql} className='iconTechnologie' alt='mysql'/>
-                <p>MySQL</p>
-            </div>
-            <div>
-                <img src={IconPython} className='iconTechnologie' alt='python'/>
-                <p>Python</p>
-            </div>
-            <div>
-                <img src={IconFlask} className='iconTechnologie' alt='flask'/>
-                <p>Flask</p>
-            </div>
-            <div>
-                <img src={IconPostgresql} className='iconTechnologie' alt='postgresql'/>
-                <p>PostgreSQL</p>
-            </div>
-        </div>
-    </div>
-  )
+            }
+        </>
+    )
 }
 
 export default Technologies
